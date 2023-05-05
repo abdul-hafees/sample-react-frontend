13c960d1673140efd4b8a3c379c77918b171acfe
Hafees <abdulhafeesk@gmail.com>
2023-05-05 17:24:49 +0400

    pagination

--- src/components/designations/DesignationList.jsx
+++ src/components/designations/DesignationList.jsx
@@ -5,103 +5,137 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import React, { useContext, useEffect, useState } from "react";
 import DesignationForm from "./DesignationForm";
-import { FaEdit, FaTrash } from 'react-icons/fa';
-
+import { FaEdit, FaTrash } from "react-icons/fa";
+import ReactPaginate from "react-paginate";
 
 const DesignationList = () => {
+  const [designations, setDesignations] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [show, setShow] = useState(false);
+  const [id, setId] = useState("");
+  const [currentPage, setCurrentPage] = useState(0);
+  const [lastPage, setLastPage] = useState('');
 
-    const [designations, setDesignations] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [show, setShow] = useState(false);
-    const [id, setId] = useState('');
 
-    const fetchDesignations = () => {
-        axios
-        .get("designations")
+  const fetchDesignations = (page=1) => {
+    axios
+      .get("designations", {params:{page:page}})
+      .then((response) => {
+        setLoading(false);
+        setDesignations(response.data.data);
+        setLastPage(response.data.last_page);
+      })
+      .catch((response) => {
+        console.log(response);
+      });
+  };
+
+  useEffect(() => {
+    fetchDesignations();
+  }, []);
+
+  const handleShow = () => {
+    setShow(true);
+  };
+
+  const editDesignation = (id) => {
+    setId(id);
+    setShow(true);
+  };
+
+  const deleteDesignation = (id) => {
+    const confirmed = window.confirm("Are you sure you want to delete?");
+
+    if (confirmed) {
+      axios
+        .delete("designations/" + id)
         .then((response) => {
-            setLoading(false);
-            console.log(response.data);
-            setDesignations(response.data)
+          fetchDesignations();
         })
         .catch((response) => {
           console.log(response);
         });
     }
+  };
 
-    useEffect(() => {
-        fetchDesignations();
-    }, []);
+  const handlePageClick = (data) => {
+    const selectedPage = data.selected + 1;
 
-    const handleShow = () => {
-        setShow(true);
-    }
+    fetchDesignations(selectedPage)
+    setCurrentPage(data.selected);
+  };
 
-    const editDesignation = (id) => {
-        setId(id)
-        setShow(true)
-    }
+  return (
+    <Container>
+      <Card>
+        <Card.Body>
+          <Card.Title className="d-flex justify-content-between">
+            Designations
+            <Button variant="primary" onClick={handleShow}>
+              Create Designation
+            </Button>
+          </Card.Title>
+          {loading ? (
+            "Loading"
+          ) : (
+            <>
+              <Table striped>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {designations.map((item, index) => (
+                    <tr key={index}>
+                      <td>{item.name}</td>
+                      <td>
+                        <FaEdit
+                          className="danger"
+                          style={{ color: "blue", cursor: "pointer" }}
+                          onClick={() => editDesignation(item.id)}
+                        />
+                      </td>
+                      <td>
+                        <FaTrash
+                          style={{ color: "red", cursor: "pointer" }}
+                          onClick={() => deleteDesignation(item.id)}
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <ReactPaginate
+                    pageCount={lastPage}
+                    onPageChange={handlePageClick}
+                    previousLabel={"previous"}
+                    nextLabel={"next"}
+                    previousClassName={"page-item"}
+                    nextClassName={"page-item"}
+                    previousLinkClassName={"page-link"}
+                    nextLinkClassName={"page-link"}
+                    pageLinkClassName={"page-link"}
+                    breakClassName={"page-item disabled"}
+                    containerClassName={"pagination"}
+                    activeClassName={"active"}
+                />
+              </div>
+            </>
+          )}
+        </Card.Body>
+      </Card>
 
-    const deleteDesignation = (id) => {
-        const confirmed = window.confirm('Are you sure you want to delete?');
-
-        if(confirmed) {
-          axios
-          .delete("designations/" + id)
-          .then((response) => {
-            fetchDesignations();
-          })
-          .catch((response) => {
-            console.log(response);
-          });
-        }
-    }
-
-
-
-    return (
-        <Container>
-            <Card>
-                <Card.Body>
-                    <Card.Title className="d-flex justify-content-between">
-                    Designations
-                    <Button variant="primary" onClick={handleShow}>
-                        Create Designation
-                    </Button>
-                    </Card.Title>
-                    {loading ? (
-                    "Loading"
-                    ) : (
-                    <Table striped>
-                        <thead>
-                        <tr>
-                            <th>Name</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {designations.map((item, index) => (
-                            <tr key={index}>
-                            <td>{item.name}</td>
-                            <td><FaEdit className="danger" style={{ color: 'blue', cursor: 'pointer' }} onClick={() => editDesignation(item.id)}/></td>
-                            <td ><FaTrash style={{ color: 'red', cursor: 'pointer' }} onClick={() => deleteDesignation(item.id)}/></td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </Table>
-                    )}
-                </Card.Body>
-            </Card>
-            
-            
-            <DesignationForm 
-                show={show}
-                setShow={setShow}
-                id={id}
-                setId={setId}
-                fetchDesignations={fetchDesignations}
-            />
-
-      </Container>
-    )
-}
+      <DesignationForm
+        show={show}
+        setShow={setShow}
+        id={id}
+        setId={setId}
+        fetchDesignations={fetchDesignations}
+      />
+    </Container>
+  );
+};
 
 export default DesignationList;
--- src/components/employees/EmployeesList.jsx
+++ src/components/employees/EmployeesList.jsx
@@ -6,9 +6,9 @@ import axios from "axios";
 import { AuthContext } from "../context/AuthContext";
 import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
-import { FaEdit, FaTrash } from 'react-icons/fa';
+import { FaEdit, FaTrash } from "react-icons/fa";
 import EmployeesForm from "./EmployeesForm";
-
+import ReactPaginate from "react-paginate";
 
 const EmployeesList = () => {
   const [employeeFormData, setEmployeeFormData] = useState({
@@ -17,29 +17,28 @@ const EmployeesList = () => {
     phone: "",
   });
 
-  const [id, setId] = useState('');
-
+  const [id, setId] = useState("");
   const [employees, setEmployees] = useState([]);
   const [loading, setLoading] = useState(false);
-  // let { token } = useContext(AuthContext);
-
+  const [currentPage, setCurrentPage] = useState(0);
+  const [lastPage, setLastPage] = useState("");
   const [show, setShow] = useState(false);
 
   const handleShow = () => {
     setShow(true);
     setEmployeeFormData({
-      name: '',
-      email: '',
-      phone: '',
+      name: "",
+      email: "",
+      phone: "",
     });
-  }
+  };
 
-  const fetchEmployees = () => {
-    setLoading(true);
+  const fetchEmployees = (page = 1) => {
     axios
-      .get("employees")
+      .get("employees", { params: { page: page } })
       .then((response) => {
-        setEmployees(response.data);
+        setEmployees(response.data.data);
+        setLastPage(response.data.last_page);
         setLoading(false);
       })
       .catch((response) => {
@@ -48,29 +47,35 @@ const EmployeesList = () => {
   };
 
   const deleteEmployee = (id) => {
-    const confirmed = window.confirm('Are you sure you want to delete?');
+    const confirmed = window.confirm("Are you sure you want to delete?");
 
-    if(confirmed) {
+    if (confirmed) {
       axios
-      .delete("employees/" + id)
-      .then((response) => {
-        fetchEmployees();
-      })
-      .catch((response) => {
-        console.log(response);
-      });
+        .delete("employees/" + id)
+        .then((response) => {
+          fetchEmployees();
+        })
+        .catch((response) => {
+          console.log(response);
+        });
     }
-  }
+  };
 
   const editEmployee = (id) => {
     setId(id);
-  }
-
-
+  };
 
   useEffect(() => {
     fetchEmployees();
   }, []);
+
+  const handlePageClick = (data) => {
+      setCurrentPage(data.selected);
+
+      const selectedPage = data.selected + 1;
+
+    fetchEmployees(selectedPage);
+  };
 
   return (
     <Container>
@@ -85,32 +90,61 @@ const EmployeesList = () => {
           {loading ? (
             "Loading"
           ) : (
-            <Table striped>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Mobile</th>
-                </tr>
-              </thead>
-              <tbody>
-                {employees.map((item, index) => (
-                  <tr key={index}>
-                    <td>{item.name}</td>
-                    <td>{item.email}</td>
-                    <td>{item.phone}</td>
-                    <td><FaEdit className="danger" style={{ color: 'blue', cursor: 'pointer' }} onClick={() => editEmployee(item.id)}/></td>
-                    <td ><FaTrash style={{ color: 'red', cursor: 'pointer' }} onClick={() => deleteEmployee(item.id)}/></td>
+            <>
+              <Table striped>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Mobile</th>
                   </tr>
-                ))}
-              </tbody>
-            </Table>
+                </thead>
+                <tbody>
+                  {employees.map((item, index) => (
+                    <tr key={index}>
+                      <td>{item.name}</td>
+                      <td>{item.email}</td>
+                      <td>{item.phone}</td>
+                      <td>
+                        <FaEdit
+                          className="danger"
+                          style={{ color: "blue", cursor: "pointer" }}
+                          onClick={() => editEmployee(item.id)}
+                        />
+                      </td>
+                      <td>
+                        <FaTrash
+                          style={{ color: "red", cursor: "pointer" }}
+                          onClick={() => deleteEmployee(item.id)}
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <ReactPaginate
+                  pageCount={lastPage}
+                  onPageChange={handlePageClick}
+                  previousLabel={"previous"}
+                  nextLabel={"next"}
+                  previousClassName={"page-item"}
+                  nextClassName={"page-item"}
+                  previousLinkClassName={"page-link"}
+                  nextLinkClassName={"page-link"}
+                  pageLinkClassName={"page-link"}
+                  breakClassName={"page-item disabled"}
+                  containerClassName={"pagination"}
+                  activeClassName={"active"}
+                />
+              </div>
+            </>
           )}
         </Card.Body>
       </Card>
 
       <EmployeesForm
-       show={show}
+        show={show}
         setShow={setShow}
         fetchEmployees={fetchEmployees}
         setEmployeeFormData={setEmployeeFormData}
@@ -120,7 +154,6 @@ const EmployeesList = () => {
       />
     </Container>
   );
-}
-
+};
 
 export default EmployeesList;
